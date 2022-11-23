4f7c41a93dec6e3f640bbc7b54a6f524c799ed15
yaginumatti <ryohei122yagi@gmail.com>
2022-11-23 15:27:14 +0900

    StringUtilsTestの修正

--- test/StringUtilsTest.java
+++ test/StringUtilsTest.java
@@ -1,5 +1,5 @@
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +10,18 @@ class StringUtilsTest {
 	@DisplayName("toSnakeCaseはスネークケースを返す_aaaの場合")
 	void toSnakeCase_aaa() {
 		assertThat(StringUtils.toSnakeCase("aaa"), is("aaa"));
+	}
+	
+	@Test
+	@DisplayName("toSnakeCaseはスネークケースを返す_HelloWorldの場合")
+	void toSnakeCase_HelloWorld() {
+		assertThat(StringUtils.toSnakeCase("HelloWorld"), is("hello_world"));
+	}
+	
+	@Test
+	@DisplayName("toSnakeCaseはスネークケースを返す_practiceJunitの場合")
+	void toSnakeCase_practiceJunit() {
+		assertThat(StringUtils.toSnakeCase("practiceJunit"), is("practice_junit"));
 	}
 
 }
